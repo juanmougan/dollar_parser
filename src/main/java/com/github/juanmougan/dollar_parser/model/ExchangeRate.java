4a434c48c7ce3be3307668fc6097136fd1336f59
Juan Manuel Mougan <juanmougan@gmail.com>
2017-01-17 17:47:32 -0300

    Added some structure to the ExchangeRate class

--- src/main/java/com/github/juanmougan/dollar_parser/model/ExchangeRate.java
+++ src/main/java/com/github/juanmougan/dollar_parser/model/ExchangeRate.java
@@ -1,5 +1,39 @@
 package com.github.juanmougan.dollar_parser.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class ExchangeRate {
-	// TODO, maybe Currency (enum) originalCurrency, BigDecimal rate, a timestamp?
+    private Currency originalCurrency;
+
+    private BigDecimal rate;
+
+    private LocalDateTime timestamp;
+
+    public ExchangeRate() {
+    }
+
+    public Currency getOriginalCurrency() {
+        return originalCurrency;
+    }
+
+    public void setOriginalCurrency(Currency originalCurrency) {
+        this.originalCurrency = originalCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
